28b30c42e5870fb6f586b5280a9f63db48bc1b19
Roarco <sebuscaroberth@gmail.com>
2022-06-17 15:16:11 -0500

    solucionando problemas con deploy

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5050;
 
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
